b93b139faf8b3c717239618ad25656f81d18ed92
Maksym Kopak <maksym.kopak@develops.today>
2026-03-30 13:10:24 +0300

    feat: changed logs message and added plannerId check

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -1,5 +1,11 @@
 import { Router, Request, Response } from "express";
 
+function isPlannerOrder(order: any) {
+  return order.line_items?.some((item: any) =>
+    item.properties?.some((p: any) => p.name === "subscriptionPlannerId"),
+  );
+}
+
 const router = Router();
 
 router.post("/orders-paid", async (req: Request, res: Response) => {
@@ -9,9 +15,30 @@ router.post("/orders-paid", async (req: Request, res: Response) => {
 
   try {
     const order = JSON.parse(rawBody.toString());
-    console.log("[TEST] RAW ORDER PAYLOAD:", JSON.stringify(order, null, 2));
+
+    console.log("Order received:", order.id);
+
+    const tags = order.tags ? order.tags.split(",") : [];
+
+    if (!isPlannerOrder(order)) {
+      console.log("Skip: not planner");
+      return;
+    }
+
+    if (tags.includes("SPLIT_FROM")) {
+      console.log("Skip: already split");
+      return;
+    }
+
+    console.log("Planner order detected");
+
+    console.log("Line items:", order.line_items?.length);
+    console.log(
+      "🧾 Properties:",
+      order.line_items?.map((i: any) => i.properties),
+    );
   } catch (err) {
-    console.error("[TEST] Failed to parse payload:", err);
+    console.error("ERROR:", err);
   }
 });
 
