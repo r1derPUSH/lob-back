990e34fbbf189d1e26addb6e49c5054e3a1b53c0
Maksym Kopak <maksym.kopak@develops.today>
2026-04-01 14:58:15 +0300

    feat: update cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import webhookRouter from "./routes/webhook";
 import plannerRouter from "./routes/planner";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "https://fortheloveofbread.ae",
+      "https://www.fortheloveofbread.ae",
+    ],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  }),
+);
+
 const PORT = process.env.PORT || 3000;
 
 // Use raw body for webhook HMAC verification
